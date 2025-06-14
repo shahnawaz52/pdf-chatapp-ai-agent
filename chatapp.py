3416e6c298ec75ced9cfc3199056ff23ae47962b
sid-bluemax <sid@bluemaxpartners.com>
2025-06-14 12:39:48 -0500

    implement model to get information from document

---
+++ chatapp.py
@@ -0,0 +1,113 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+from langchain.chat_models import ChatOpenAI 
+import torch
+from dotenv import load_dotenv
+
+
+load_dotenv()
+os.environ["STREAMLIT_SERVER_ENABLE_FILE_WATCHER"] = "false" 
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+HF_TOKEN = os.getenv("HF_TOKEN")
+torch.classes.__path__ = [] 
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=50000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2",  # or any other HF model you want
+        model_kwargs={'device': 'cpu'}  # or 'cuda' if you have a GPU
+    )
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatOpenAI(
+        openai_api_base="https://api.groq.com/openai/v1",
+        openai_api_key=os.getenv("GROQ_API_KEY"),
+        model_name="llama3-8b-8192",
+        temperature=0.3
+    )
+
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+def user_input(user_question):
+    index_path = "faiss_index"
+    if not os.path.exists(index_path):
+        st.warning("Please upload and process PDF files first!")
+        return
+
+    # embeddings = HuggingFaceEmbeddings(
+    #     model_name="Qwen/Qwen3-Embedding-8B",
+    #     model_kwargs={"token": os.getenv("HF_TOKEN")}
+    # )
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    # 4. Load the FAISS index with the embedding function, not the embedding vector
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    # 5. Perform similarity search (this will use the embedding function internally)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs=True
+    )
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+
+def main():
+    st.set_page_config("Multi PDF Chatbot", page_icon = ":scroll:")
+    st.header("PDF's 📚 - Chat Agent 🤖 ")
+
+    user_question = st.text_input("Ask a Question from the PDF Files uploaded .. ✍️📝")
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+
+        # st.image("img/Robot.jpg")
+        st.write("---")
+        
+        st.title("📁 PDF File's Section")
+        pdf_docs = st.file_uploader("Upload your PDF Files & \n Click on the Submit & Process Button ", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."): # user friendly message.
+                raw_text = get_pdf_text(pdf_docs) # get the pdf text
+                text_chunks = get_text_chunks(raw_text) # get the text chunks
+                get_vector_store(text_chunks) # create vector store
+                st.success("Done")
+
+if __name__ == "__main__":
+    main()
